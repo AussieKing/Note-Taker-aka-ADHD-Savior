e3378fdbe403e2bf54ffd1e9bba24ebd0b8ef0be
AussieKing <126050763+AussieKing@users.noreply.github.com>
2023-06-07 15:53:51 +1000

    fixed the delete request (was updating the db.json but not the updated notes from the server

--- server.js
+++ server.js
@@ -4,63 +4,73 @@ const DEV_PORT = 3001;
 const PORT = process.env.PORT || DEV_PORT;
 const express = require('express');
 const app = express();
-const fs = require('fs'); // can use 'npm install -g nodemon' and 'nodemon server.js'
-const path = require('path'); // can use 'npm install -g nodemon' and 'nodemon server.js'
-const uuid = require('uuid'); // to generate unique id for each note
-const notes = require('./db/db.json'); // to read the db.json file
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
 const dbPath = './db/db.json';
+
+// Create the db.json file if it doesn't exist
 if (!fs.existsSync(dbPath)) {
-    fs.writeFileSync(dbPath, '[]');
-} // if the db.json file does not exist, create it with an empty array
-try {
-    JSON.parse(fs.readFileSync(dbPath));
-} catch (err) {
-    fs.writeFileSync(dbPath, '[]');
-} // if not a JSON file, create an empty array
+  fs.writeFileSync(dbPath, '[]');
+}
 
-//! MIDDLEWARE (to parse incoming request bodies in a middleware before your handlers, available under the req.body property)
-app.use(express.urlencoded({ extended: true })); //setup the middleware via the built in function in Express
-app.use(express.json()); // adds middleware to parse JSON
-app.use(express.static('public')); // to use the directory from which we we serve the static files
+//! MIDDLEWARE (to parse incoming request bodies in a middleware before your handlers, available under the req.body property)app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-//! GET request 
-// (using req and res to send and receive data and the async/await keyword to wait for the response without stopping the execution of the code)
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-}); // If no matching route is found default to home
+// Read the notes from db.json
+function readNotes() {
+  const notesData = fs.readFileSync(dbPath, 'utf-8');
+  return JSON.parse(notesData);
+}
 
-//! POST request 
+// Write the notes to db.json
+function writeNotes(notes) {
+  fs.writeFileSync(dbPath, JSON.stringify(notes));
+}
+
+//! GET request routes
 // (using req and res to send and receive data)
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuid.v4();
-    notes.push(newNote);
-    res.json(newNote);
-
-    fs.writeFileSync(dbPath, JSON.stringify(notes)); // adding the new note to the data array
-    console.log('New note added to the database 📝');
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-//! DELETE request - ** optional **
-// This was a bonus for the assignment: "DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file"
-app.delete('/api/notes/:id', (req, res) => {
-    const id = req.params.id; // getting the notes with relative id from the request parameters
-    const notesNew = notes.filter((note) => note.id !== id); // filtering the notes array to get the notes with id different from the one we want to delete
+app.get('/api/notes', (req, res) => {
+  const notes = readNotes();
+  res.json(notes);
+});
 
-    fs.writeFileSync(dbPath, JSON.stringify(notesNew));
-    notes = notesNew;
-    console.log('Note deleted from the database 🗑️');
-    res.json(notesNew);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//! POST request route
+// (using req and res to send and receive data)
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  const notes = readNotes();
+  notes.push(newNote);
+  writeNotes(notes);
+  console.log('New note added to the database 📝');
+  res.json(newNote);
+});
+
+//! DELETE request route ** OPTIONAL**
+// This was a bonus for the assignment: "DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file"
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  let notes = readNotes();
+  const notesNew = notes.filter((note) => note.id !== id);
+  writeNotes(notesNew);
+  console.log('Note deleted from the database 🗑️');
+  res.json(notesNew);
 });
 
 //! LISTENER 
 // to listen to the user requests on local PORT
 app.listen(PORT, () => {
-console.log(`App now listening on PORT: ${PORT}`)
+  console.log(`App now listening on PORT: ${PORT}`);
 });
